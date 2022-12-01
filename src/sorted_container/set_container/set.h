a99eead154e4506100ee3585cd67af27a3ffb63f
skiflok <anisimov.a.v@inbox.ru>
2022-12-01 23:07:50 +0300

    start set

--- src/sorted_container/set_container/set.h
+++ src/sorted_container/set_container/set.h
@@ -2,7 +2,26 @@
 // Created by Lok on 17.11.2022.
 //
 
+
+//#include <cstddef>
+//#include <initializer_list>
+#include <iostream>
+//#include <utility>
+//#include <valarray>
+
 #ifndef S21_CONTAINERS_SRC_SORTED_CONTAINER_SET_CONTAINER_SET_H_
 #define S21_CONTAINERS_SRC_SORTED_CONTAINER_SET_CONTAINER_SET_H_
 
-#endif //S21_CONTAINERS_SRC_SORTED_CONTAINER_SET_CONTAINER_SET_H_
+namespace s21 {
+
+template <class Key, class Compare = std::less<Key>,
+          class Allocator = std::allocator<Key>>
+
+class S21Set{
+  using key_type = Key;
+};
+
+
+}
+
+#endif // S21_CONTAINERS_SRC_SORTED_CONTAINER_SET_CONTAINER_SET_H_
---
+++ tests/test_set.cc
@@ -0,0 +1,17 @@
+//
+// Created by Lok on 01.12.2022.
+//
+
+#include <gtest/gtest.h>
+
+// #include <utility>
+#include <set>
+
+#include "../src/sorted_container/set_container/set.h"
+
+using namespace s21;
+
+class S21Vector_test : public ::testing::Test {
+protected:
+  std::set<int> set_empty;
+};
