b5bb128444b7373b0026ddc20f6fc199d98a67e0
skiflok <anisimov.a.v@inbox.ru>
2022-12-03 11:50:37 +0300

    start set

--- src/sorted_container/set_container/set.h
+++ src/sorted_container/set_container/set.h
@@ -18,7 +18,22 @@ template <class Key, class Compare = std::less<Key>,
           class Allocator = std::allocator<Key>>
 
 class S21Set{
+  class SetIterator<Key>;
   using key_type = Key;
+  using value_type = Key;
+  using reference = value_type &;
+  using const_reference = const value_type &;
+  using iterator = SetIterator<value_type>;
+  using const_iterator = SetConstIterator<value_type>;
+  using size_type = size_t;
+
+
+  class SetIterator<Key> {
+
+
+  };
+
+
 };
 
 
