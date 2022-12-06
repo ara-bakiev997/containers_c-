217f6e8cdb9c387c531dd885d8637b3ae929a060
skiflok <anisimov.a.v@inbox.ru>
2022-12-06 23:45:34 +0300

    GetRoot

--- tests/test_RBT.cc
+++ tests/test_RBT.cc
@@ -10,11 +10,18 @@ using namespace s21;
 class Tree_test : public ::testing::Test {
 protected:
   std::set<int> set_empty;
-  Tree<int> tree_;
+  Tree<int> tree_null_;
+  Node<int> node_null;
+//  Node<int> node_data(1);
 
 };
 
-TEST_F(Tree_test, first) {
-  EXPECT_EQ(tree_.GetRoot(), nullptr);
 
+TEST_F(Tree_test, first) {
+  EXPECT_EQ(tree_null_.GetRoot(), nullptr);
+  EXPECT_EQ(tree_null_.GetRoot()->data_, nullptr);
+  EXPECT_EQ(tree_null_.GetRoot()->parent_, nullptr);
+  EXPECT_EQ(tree_null_.GetRoot()->left_, nullptr);
+  EXPECT_EQ(tree_null_.GetRoot()->right_, nullptr);
+  EXPECT_EQ(tree_null_.GetRoot()->color_, BLACK);
 }
