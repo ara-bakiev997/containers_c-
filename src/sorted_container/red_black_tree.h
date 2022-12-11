b9a523ad9421621b2b6a58ef5a773cc7fc316768
skiflok <anisimov.a.v@inbox.ru>
2022-12-11 13:28:33 +0300

    fix comments

--- src/sorted_container/red_black_tree.h
+++ src/sorted_container/red_black_tree.h
@@ -21,70 +21,8 @@ template <class T> struct RBT {
   RBT<T> *left_{};
   RBT<T> *right_{};
   enum RBT_colors color_ = BLACK;
-  bool is_empty_ = true;
   T data_;
-  //    RBT()
-  //        : parent_(nullptr), left_(nullptr), right_(nullptr), color_(BLACK),
-  //          data_(), is_empty_(true) {}
-  //    explicit RBT(const T &data)
-  //        : parent_(nullptr), left_(nullptr), right_(nullptr), color_(RED),
-  //          data_(data), is_empty_(false) {}
 
-  //    RBT()= default;
-  //    explicit RBT(const T &data) : color_(RED), data_(data), is_empty_(false)
-  //    {}
-
-  //  RBT &operator=(const RBT &other) {
-  //    if (this != &other) {
-  //      this->parent_ = other.parent_;
-  //      this->color_ = other.color_;
-  //      this->data_ = other.data_;
-  //      this->left_ = other.left_;
-  //      this->right_ = other.right_;
-  //    }
-  //    return *this;
-  //  }
-
-  //      RBT &operator=(RBT &&other)  noexcept {
-  //  //      if (this != other) {
-  //          std::swap(this, other);
-  //    //      this->parent_ = other.parent_;
-  //    //      this->color_ = other.color_;
-  //    //      this->data_ = other.data_;
-  //    //      this->left_ = other.left_;
-  //    //      this->right_ = other.right_;
-  //  //      }
-  //        return *this;
-  //      }
-
-  void Insert(const T data) {
-
-    if (this == nullptr) {
-      //      this = new RBT<T>;
-      //      std::swap(this, &temp);
-      //      this->is_empty_ = false;
-      return;
-    } else if (this->is_empty_) {
-      this->data_ = data;
-      this->is_empty_ = false;
-      return;
-    } else if (this->data_ > data) {
-      this->right_ = new RBT<T>;
-      right_->data_ = data;
-    } else if (this->data_ < data) {
-      this->left_->Insert(data);
-    }
-  }
-
-  void InsertRBT() {}
-
-  void RBTPrint() {
-    if (this == nullptr)
-      return;
-    this->right_->RBTPrint();
-    std::cout << this->data_ << std::endl;
-    this->left_->RBTPrint();
-  }
 };
 
 template <class T> class Tree {
@@ -100,18 +38,13 @@ public:
 
   ~Tree() = default;
 
-  void fu() {}
 
   void InsertRBT(RBT<T> *&paste_here, RBT<T> *&node) {
     if (paste_here == nullptr) {
       std::swap(paste_here, node);
-      //      std::swap(this->root_, temp); // init
     } else if (node->data_ > paste_here->data_) {
-      // right
       InsertRBT(paste_here->right_, node);
-      //      this->root_->right_ = temp;
     } else if (node->data_ < paste_here->data_) {
-      // left
       InsertRBT(paste_here->left_, node);
     }
   }
@@ -119,21 +52,7 @@ public:
   void Insert(const T &data) {
     auto *temp = new RBT<T>;
     temp->data_ = data;
-
     InsertRBT(root_, temp);
-
-    //    if (this->root_ == nullptr) {
-    //      //      this->root_ = temp;
-    //      InsertRBT(this->root_, temp);
-    ////      std::swap(this->root_, temp); // init
-    //    } else if (temp->data_ > this->root_->data_) {
-    //      // right
-    //      InsertRBT(this->root_->right_, temp);
-    //      //      this->root_->right_ = temp;
-    //    } else if (temp->data_ < this->root_->data_) {
-    //      // left
-    //      InsertRBT(this->root_->left_, temp);
-    //    }
   }
 
   void RBTPrint(RBT<T> *&node) {
