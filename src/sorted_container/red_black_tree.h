a45e44be484345ed658e1eb8c7421677e646437e
skiflok <anisimov.a.v@inbox.ru>
2022-12-18 16:39:19 +0300

    print RBT

--- src/sorted_container/red_black_tree.h
+++ src/sorted_container/red_black_tree.h
@@ -63,28 +63,73 @@ public:
   void WalkInWidth() {
     std::queue<RBT<T> *> queue;
     queue.push(this->root_);
-    int count = 1;
+    int count = 0;
     int sons = 1;
+    RBT<T> *fake;
 
     while (!queue.empty()) {
       RBT<T> *temp;
       temp = queue.front();
       queue.pop();
-      if (temp != nullptr) {
-        std::cout << temp->data_ << std::endl;
+      if (temp != fake) {
+        std::cout << temp->data_ << " ";
       } else {
-
+        std::cout << "null"
+                  << " ";
+      }
+      ++count;
+      if (count == sons) {
+        std::cout << std::endl;
+        count = 0;
+        sons *= 2;
       }
 
-      if (temp->left_ != nullptr)
-        queue.push(temp->left_);
-      else
-        queue.push(nullptr);
-      if (temp->right_ != nullptr)
-        queue.push(temp->right_);
-      else
-        queue.push(nullptr);
+      if (temp != fake) {
+        if (temp->left_ != nullptr) {
+          queue.push(temp->left_);
+        } else {
+          queue.push(fake);
+        }
+
+        if (temp->right_ != nullptr) {
+          queue.push(temp->right_);
+        } else {
+          queue.push(fake);
+        }
+
+      }
     }
+  }
+
+  // Function to print binary tree in 2D
+  // It does reverse inorder traversal
+  void print2DUtil(RBT<T>* root, int space)
+  {
+    // Base case
+    if (root == NULL)
+      return;
+
+    // Increase distance between levels
+    space += 5;
+
+    // Process right child first
+    print2DUtil(root->right_, space);
+
+    // Print current node after space
+    // count
+    std::cout << std::endl;
+    for (int i = 5; i < space; i++)
+      std::cout << " ";
+    std::cout << root->data_ << "\n";
+
+    // Process left child
+    print2DUtil(root->left_, space);
+  }
+
+  // Wrapper over print2DUtil()
+  void print2D() {
+    // Pass initial space count as 0
+    print2DUtil(this->root_, 0);
   }
 
   void RBTPrint(RBT<T> *&node) {
@@ -100,7 +145,7 @@ public:
   RBT<T> *GetRoot() { return this->root_; }
 
 private:
-  RBT<T> *root_;
+  RBT<T> *root_{};
 };
 } // namespace s21
 
